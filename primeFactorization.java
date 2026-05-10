caac4496c3cf5fed602174b197198f6c68861401
mjithendragouda <mjithendragouda@gmail.com>
2026-05-10 12:32:49 +0530

    scanner is closed

--- primeFactorization.java
+++ primeFactorization.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 public class primeFactorization {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Provide  me a number: ");
-    int num = sc.nextInt();
-    int n=num;
-    for(int i=2; i<=Math.sqrt(n); i++){
-        while(n%i==0){
-            n=n/i;
-            
-            System.out.print(i+" ");
-           // System.out.println(q);
-        }      
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide  me a number: ");
+        int num = sc.nextInt();
+        int n = num;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                n = n / i;
+                System.out.print(i + " ");
+            }
+        }
+        if (n != 1) {
+            System.out.print(n);
+        }
+        sc.close();
     }
-    if(n!=1){
-        System.out.print(n);
-    }
-}
-    
+
 }
