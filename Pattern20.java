bf0f7839baffbbc3bd0a9b0f1dc5152124e54d77
mjithendragouda <mjithendragouda@gmail.com>
2026-05-25 06:16:34 +0530

    Final Pattern

---
+++ Pattern20.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an odd number: ");
+        int n = sc.nextInt();
+        int mid = n/2+1;
+        int k = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= mid) {
+                    if (j == 1 || j == n || j == k || j == i) {
+                        System.out.print("*\t");
+                    } else
+                        System.out.print("\t");
+                } else {
+                    if (j == 1 || j == n) {
+                        System.out.print("*\t");
+                    } else
+                        System.out.print("\t");
+                }
+            }
+            System.out.println();
+            k--;
+        }
+        sc.close();
+    }
+}
