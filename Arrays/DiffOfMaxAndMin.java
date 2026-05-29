933f185553ca2cdbd76f5723d9cf1eaf5c538d98
mjithendragouda <mjithendragouda@gmail.com>
2026-05-29 19:48:23 +0530

    array pragrams initiated

---
+++ Arrays/DiffOfMaxAndMin.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DiffOfMaxAndMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int diffrence = diffrence(arr);
+        System.err.println("Diffrence of Max and Min val in the array is: "+diffrence);
+        sc.close();
+    }
+    public static int diffrence(int[] arr){
+        int diffrence = 0;
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]>=min){
+                if(arr[i]>=max){
+                    max = arr[i];
+                }
+            }else{
+                min = arr[i];
+            }
+        }
+        diffrence = max - min;
+        return diffrence;
+    }
+}
