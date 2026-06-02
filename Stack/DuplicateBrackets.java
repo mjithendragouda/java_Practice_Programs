fd814ed62c4ec563661b45e0d0b5d8e9d7e566db
mjithendragouda <mjithendragouda@gmail.com>
2026-06-02 11:03:21 +0530

    stack - Duplicate brackets

---
+++ Stack/DuplicateBrackets.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DuplicateBrackets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write an expression: ");
+        String str = sc.nextLine();
+        Stack<Character> st = new Stack<>();
+        boolean duplicateBrackets = false;
+        
+        
+        for(int i = 0 ;i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == ')'){
+                if(!st.isEmpty() && st.peek()=='('){
+                   duplicateBrackets = true; 
+                   System.out.println(duplicateBrackets);
+                   return;  
+                }
+                while (!st.isEmpty() && st.peek() != '(') {
+                    st.pop();
+                }
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                
+            }else{
+                st.push(ch);
+            }
+
+        }           
+        System.out.println(duplicateBrackets);
+        sc.close();
+        
+    }
+}
